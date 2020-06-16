f2904d5856d9517b444b48753b83fa4a860680f2
Tedros <tedtfu@gmail.com>
2020-06-16 18:25:59 -0400

    dynamic port added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const app = require('./app');
-const port = app.get('port');
+// const port = app.get('port');
 const name = app.get('name');
+const port=process.env.PORT || 3030;
 const server = app.listen(port);
 
 server.on('listening', () =>
